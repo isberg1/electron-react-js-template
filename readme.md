7889907df94133aa5f942af5a3c508f074b241e2
isberg1 <isberg1_github@outlook.com>
2020-08-18 18:44:31 +0200

    set up Electron-React JS template

---
+++ readme.md
@@ -0,0 +1,1 @@
+Electron-React Template from: https://blog.alexdevero.com/electron-and-react-app/#creating-react-app
---
+++ src/components/app.jsx
@@ -0,0 +1,16 @@
+// src/components/app.jsx
+
+// Import dependencies
+import React from "react";
+
+// Create main App component
+const App = () => (
+  <div>
+    <h1>Hello, electron!</h1>
+
+    <p>Let's start building your awesome desktop app with electron and React!</p>
+  </div>
+);
+
+// Export the App component
+export default App;
